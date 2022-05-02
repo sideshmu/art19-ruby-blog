d1293981674979ba65d8e6bb721f15cc670abb6f
Siddhant Deshmukh <sideshmu@amazon.com>
2022-05-02 02:08:32 +0000

    Added Uniqueness Validation to Articles titles

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   
     has_many :comments, dependent: :destroy
   
-    validates :title, presence: true
+    validates :title, presence: true, uniqueness: true
     validates :body, presence: true, length: { minimum: 10 }
   end
   
