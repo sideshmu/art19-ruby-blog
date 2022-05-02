4697a464ade148bd9c85d35edc49a64c7d47f726
Siddhant Deshmukh <sideshmu@amazon.com>
2022-05-02 02:02:12 +0000

    Updated APIs to receive/render JSON instead of HTML

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,45 +1,92 @@
 class ArticlesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  ##
+  # Retrieve all articles
+  # GET /articles/
   def index
     @articles = Article.all
+    render json: @articles, status: :ok
   end
 
+  ##
+  # Retrieve article by :id
+  # GET /articles/:id
   def show
-    @article = Article.find(params[:id])
+    @article = Article.find_by_id(params[:id])
+
+    if @article
+      render json: @article, status: :ok
+    else
+      render json: @article, status: :unprocessable_entity
+    end
   end
 
-  def new
-    @article = Article.new
-  end
-
+  # ##
+  # # Don't need for json-api currently
+  # # Retrieve article by :id
+  # # GET /articles/new
+  # def new
+  #   @article = Article.new
+  # end
+  
+  ##
+  # Create new article
+  # POST /articles
+  # JSON Data:
+  # {
+  #   "title": "Example Title",
+  #   "body": "Example Body which is long"
+  # }
   def create
     @article = Article.new(article_params)
-
     if @article.save
-      redirect_to @article
+      render json: @article, status: :ok
     else
-      render :new, status: :unprocessable_entity
+      render json: @article.errors, status: :unprocessable_entity
     end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  # ##
+  # # Don't need for json-api currently
+  # # Edit article by :id
+  # # GET /articles/:id/edit
+  # def new
+  #   @article = Article.new
+  # end
+  # def edit
+  #   @article = Article.find(params[:id])
+  # end
 
+  ##
+  # Edit article
+  # PATCH/PUT /articles/:id
+  # JSON Data:
+  # {
+  #   "title": "Example Title",
+  #   "body": "Example Body which is long",
+  #   "status": "public"
+  # }
   def update
-    @article = Article.find(params[:id])
+    @article = Article.find_by_id(params[:id])
 
-    if @article.update(article_params)
-      redirect_to @article
+    if @article and @article.update(article_params)
+      render json: @article, status: :ok
     else
-      render :edit, status: :unprocessable_entity
+      render json: @article, status: :unprocessable_entity
     end
   end
 
+  ##
+  # Destroy article
+  # DELETE /articles/:id
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
+    @article = Article.find_by_id(params[:id])
 
-    redirect_to root_path, status: :see_other
+    if @article and @article.destroy
+      render json: {msg: "Deleted Article #{params[:id]} successfully!"}, status: :ok
+    else
+      render json: @article, status: :unprocessable_entity
+    end
   end
 
   private
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,42 @@
 class CommentsController < ApplicationController
-    def create
-      @article = Article.find(params[:article_id])
-      @comment = @article.comments.create(comment_params)
-      redirect_to article_path(@article)
+  skip_before_action :verify_authenticity_token
+  ##
+  # Create new comment on an article
+  # POST /articles/:article_id/comments
+  # JSON Data:
+  # {
+  #   "commenter": "Example Commenter",
+  #   "body": "Example Comment Body",
+  #   "status": "example status"
+  # }
+  def create
+    @article = Article.find_by_id(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+
+    if @article and @comment.persisted?
+      render json: {article: @article, article_comments: @article.comments}, status: :ok
+    else
+      render json: @article, status: :unprocessable_entity
     end
-  
-    def destroy
-      @article = Article.find(params[:article_id])
-      @comment = @article.comments.find(params[:id])
-      @comment.destroy
-      redirect_to article_path(@article), status: 303
+  end
+
+  ##
+  # Destroy comment on an article
+  # DELETE /articles/:article_id/comments/:id
+  def destroy
+    @article = Article.find_by_id(params[:article_id])
+    @comment = @article.comments.find_by_id(params[:id])
+
+    if @article and @comment and @comment.destroy
+      render json: {msg: "Deleted Comment #{params[:id]} successfully!"}, status: :ok
+    else
+      render json: @article, status: :unprocessable_entity
     end
-  
-    private
-      def comment_params
-        params.require(:comment).permit(:commenter, :body, :status)
-      end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body, :status)
+    end
   end
   
