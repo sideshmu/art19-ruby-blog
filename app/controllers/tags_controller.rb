a9af9c34c953125b915474579ddc60703080b7c7
Siddhant Deshmukh <sideshmu@amazon.com>
2022-05-03 09:22:31 +0000

    Added CRUD operations for Tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,95 +2,102 @@ class TagsController < ApplicationController
   skip_before_action :verify_authenticity_token
   ##
   # Retrieve all tags after filtering 
-  # GET /tags?query=<query>
+  # If `article_id` is provided, will limit search to that article only
+  # GET /tags?query=<query>&optional_article_id=<optional_article_id>
   def index
-    @tags = Tag.where("title LIKE ?", "%#{params[:query]}%")
+    unless params[:article_id].nil?
+      @tags = Article.find_by_id(params[:article_id]).tags.where("title LIKE ?", "%#{params[:query]}%")
+    else
+      @tags = Tag.where("title LIKE ?", "%#{params[:query]}%")
+    end
     render json: @tags, status: :ok
   end
 
   ##
-  # Retrieve article by :id
-  # GET /articles/:id
+  # Retrieve tag by :id
+  # GET /tags/:id
   def show
-    @article = Article.find_by_id(params[:id])
+    @tag = Tag.find_by_id(params[:id])
 
-    if @article
-      render json: @article, status: :ok
+    if @tag
+      render json: @tag, status: :ok
     else
-      render json: @article, status: :unprocessable_entity
+      render json: @tag, status: :unprocessable_entity
     end
   end
-
-  # ##
-  # # Don't need for json-api currently
-  # # Retrieve article by :id
-  # # GET /articles/new
-  # def new
-  #   @article = Article.new
-  # end
   
   ##
-  # Create new article
-  # POST /articles
+  # Create new tag ([optionally for an article])
+  # POST /tags
   # JSON Data:
   # {
   #   "title": "Example Title",
-  #   "body": "Example Body which is long"
+  #   "counter": 1,
+  #   "article_id": <article_id>    #Optional
   # }
   def create
-    @article = Article.new(article_params)
-    if @article.save
-      render json: @article, status: :ok
+    @article = Article.find_by_id(params[:article_id]) 
+
+    if @article
+      @tag = @article.tags.create(tag_params)
+      if @tag.persisted?
+        render json: @tag, status: :ok
+      else
+        render json: @tag.errors, status: :unprocessable_entity
+      end
     else
-      render json: @article.errors, status: :unprocessable_entity
+      @tag = Tag.new(tag_params)
+      if @tag.save
+        render json: @tag, status: :ok
+      else
+        render json: @tag.errors, status: :unprocessable_entity
+      end
     end
   end
 
-  # ##
-  # # Don't need for json-api currently
-  # # Edit article by :id
-  # # GET /articles/:id/edit
-  # def new
-  #   @article = Article.new
-  # end
-  # def edit
-  #   @article = Article.find(params[:id])
-  # end
-
   ##
-  # Edit article
-  # PATCH/PUT /articles/:id
+  # Edit tag 
+  # PATCH/PUT /tags/:id
   # JSON Data:
   # {
   #   "title": "Example Title",
-  #   "body": "Example Body which is long",
-  #   "status": "public"
+  #   "counter": 1,
+  #   "article_id": <article_id>    #Optional
   # }
   def update
-    @article = Article.find_by_id(params[:id])
+    @article = Article.find_by_id(params[:article_id])
+    @tag = Tag.find_by_id(params[:id])
 
-    if @article and @article.update(article_params)
-      render json: @article, status: :ok
+    if @article
+      if @tag and @tag.update(tag_params) and @article.tags.update(tag_params)
+        render json: @tag, status: :ok
+      else
+        render json: @tag.errors, status: :unprocessable_entity
+      end
     else
-      render json: @article, status: :unprocessable_entity
+      if @tag and @tag.update(tag_params)
+        render json: @tag, status: :ok
+      else
+        render json: @tag, status: :unprocessable_entity
+      end
     end
   end
 
   ##
-  # Destroy article
-  # DELETE /articles/:id
+  # Destroy tag
+  # DELETE /tag/:id
   def destroy
-    @article = Article.find_by_id(params[:id])
+    @tag = Tag.find_by_id(params[:id])
 
-    if @article and @article.destroy
-      render json: {msg: "Deleted Article #{params[:id]} successfully!"}, status: :ok
+    if @tag and @tag.destroy
+      render json: {msg: "Deleted Tag #{params[:id]} successfully!"}, status: :ok
     else
-      render json: @article, status: :unprocessable_entity
+      render json: @tag.errors, status: :unprocessable_entity
     end
   end
 
   private
     def tag_params
-      params.require(:tag).permit(:title, :counter, :query)
+      params.require(:tag).permit(:title, :counter)
     end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,15 @@
 class Tag < ApplicationRecord
-    has_and_belongs_to_many :articles
+  has_and_belongs_to_many :articles, :dependent => :restrict_with_error
+
+  validates :title, presence: true
+  validates :counter, presence: true, numericality: { only_integer: true }
+    
+  before_destroy :allow_destroy
+  private 
+    def allow_destroy
+      unless self.articles.empty?
+        self.errors.add('Cannot_delete','In use by Articles')
+        throw(:abort)
+      end
+    end
 end
