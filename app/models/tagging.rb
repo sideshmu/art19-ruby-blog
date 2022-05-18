97e4d3780d31962e5e3086721fad39f94014f6dc
Siddhant Deshmukh <sideshmu@amazon.com>
2022-05-18 02:47:35 +0000

    Added Taggings controller, routes and validations

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -2,4 +2,24 @@ class Tagging < ApplicationRecord
   # belongs_to :tag, counter_cache: taggings_count  
   belongs_to :tag, counter_cache: true
   belongs_to :article
+
+  validates :article_id, presence: true
+  validates :tag_id, presence: true
+
+  before_save  :tag_exists?, :article_exists?
+
+  private
+    def tag_exists?
+      unless Tag.exists?(id: tag_id)
+        errors.add(:base, 'tag does not exist')
+        throw(:abort)
+      end
+    end
+
+    def article_exists?
+      unless Article.exists?(id: article_id)
+        errors.add(:base, 'article does not exist')
+        throw(:abort)
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
 
   resources :tags
-  
+  resources :taggings
   # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
