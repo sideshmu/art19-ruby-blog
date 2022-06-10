52d11a7179a006735f7bbe623a07e5e49026b818
Siddhant Deshmukh <sideshmu@amazon.com>
2022-06-10 06:37:35 +0000

    fixed articles model spec to test each validation independently and also test error messages

--- spec/models/articles_spec.rb
+++ spec/models/articles_spec.rb
@@ -11,34 +11,38 @@ RSpec.describe Article, type: :model do
     }
   end
 
-  let(:invalid_article_attributes) do
-    {
-      title: 'Test title',
-      body: 'shortbody',
-      status: 'invalid_status'
-    }
-  end
-
-  context 'verify article creation' do
-    let(:article) { Article.create(valid_article_attributes) }
+  context 'when article is created' do
+    let(:article)        { Article.create(valid_article_attributes) }
+    let(:invalid_body)   { Article.create(body: 'shortbody') }
+    let(:invalid_status) { Article.create(status: 'invalid_status') }
 
     it 'article is valid with valid attributes' do
       expect(article).to be_valid
     end
 
-    let(:invalid_article) { Article.create(invalid_article_attributes) }
+    it 'article is invalid with invalid body' do
+      expect(invalid_body).not_to be_valid
+    end
 
-    it 'article is invalid with invalid attributes' do
-      expect(invalid_article).not_to be_valid
+    it 'article gives errors for invalid body' do
+      expect(invalid_body.errors.full_messages_for(:body)).to match(['Body is too short (minimum is 10 characters)'])
+    end
+
+    it 'article is invalid with invalid status' do
+      expect(invalid_status).not_to be_valid
+    end
+
+    it 'article gives errors for invalid status' do
+      expect(invalid_status.errors.full_messages_for(:status)).to match(['Status is not included in the list'])
     end
   end
 
-  context 'verify uniqueness of article title' do
+  context 'when identical articles are created' do
     let(:article) { build(:article, title: 'identical') }
 
     before { create(:article, title: 'identical') }
 
-    it 'article with same title is not created' do
+    it 'creating article with same title is invalid' do
       expect(article).to be_invalid
     end
   end
