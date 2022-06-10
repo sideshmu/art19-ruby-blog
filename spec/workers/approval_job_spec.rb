a82249adfe48a364d48550f08f882188d9ac5583
Siddhant Deshmukh <sideshmu@amazon.com>
2022-06-10 05:59:41 +0000

    Added approval job spec, deleted from Comments controller spec

---
+++ spec/workers/approval_job_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ApprovalJob, type: :worker do
+  let(:article)      { create(:article) }
+  let(:good_comment) { build(:comment, body: 'good comment') }
+  let(:bad_comment)  { build(:comment, body: 'yoinks !') }
+
+  it { is_expected.to be_a Sidekiq::Job }
+
+  context 'verify comment approval process' do
+    before do
+      article.comments << good_comment
+      article.comments << bad_comment
+    end
+
+    it 'comment is approved when no trigger words found' do
+      expect(good_comment.approval).to match('submitted')
+      ApprovalJob.new.perform(good_comment.id)
+      good_comment.reload
+      expect(good_comment.approval).to match('approved')
+    end
+
+    it 'comment is flagged when trigger words found' do
+      expect(bad_comment.approval).to match('submitted')
+      ApprovalJob.new.perform(bad_comment.id)
+      bad_comment.reload
+      expect(bad_comment.approval).to match('flagged')
+    end
+  end
+end
